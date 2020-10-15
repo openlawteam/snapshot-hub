b409d4556fdb6695aad3dcfda1478cecd8324d20
Felipe Forbeck <felipe.forbeck@gmail.com>
2020-10-15 12:05:57 -0300

    Added health check

--- index.ts
+++ index.ts
@@ -13,5 +13,5 @@ export default (app, server) => {
   app.use(cors());
   app.use('/api', api);
   // @ts-ignore
-  app.get('/*', (req, res) => res.redirect('https://snapshot.page/#/balancer'));
+  app.get('/*', (req, res) => res.sendStatus(200));
 }
