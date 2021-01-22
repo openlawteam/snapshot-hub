516830733e8cf9e07d7c549730d3bf8340ba0d53
Jeremiah Naylor-Trein <jeremiahtrein@gmail.com>
2021-01-22 14:47:53 +0000

    shapes votes as object not array

--- server/helpers/adapters/postgres.ts
+++ server/helpers/adapters/postgres.ts
@@ -165,7 +165,9 @@ export const findVotesForProposals = (space, proposals) =>
   Promise.all(
     proposals.map(p =>
       getProposalVotes(space, p.id)
-        .then(votes => votes.map(toVoteMessageJson))
+        .then(votes =>
+          votes && votes.length > 0 ? toVoteMessageJson(votes) : {}
+        )
         .then(votes => {
           p['votes'] = votes;
           return p;
