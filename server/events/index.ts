961f3a1667bb925764f0d41b1a909655dadbed76
Jeremiah Naylor-Trein <jeremiahtrein@gmail.com>
2021-11-22 10:59:26 +0000

    Events v5.0.1 erc712 (#60)
    * `erc-712`: Adds `proposal` events for calling webhooks (#58)
    
    * adds `events` table to db
    
    * adds `cross-fetch` package
    
    * `adds server/events` files
    
    * adds `@types/pg` pkg
    
    * fix queries in events
    
    * imports `events` into `/index.ts`
    
    * insert events into `events` table after proposal created
    
    * adds `SERVICE_EVENTS` to `README`
    
    * adds `subscribers/local.json` to git ignore
    
    * safely parse JSON
    
    * adds `qoomon/docker-host` to docker compose file
    
    * fixes type
    
    * adds local example file for subscribers
    
    * make path for git ignored value more restrictive
    
    * removes `cross-fetch` pkg
    
    * use `axios` as it is already installed
    
    * adds config for dev and prod subscribers (#59)

---
+++ server/events/index.ts
@@ -0,0 +1,94 @@
+/**
+ * THIS FILE HAS BEEN COPIED AND MODIFIED
+ * FROM SNAPSHOT HUB FOR THE PURPOSES OF
+ * ADOPTING EVENTS AND WEBHOOKS IN THIS FORK.
+ *
+ * @see https://github.com/snapshot-labs/snapshot-hub
+ * @see https://github.com/snapshot-labs/snapshot-hub/tree/master/src/events
+ */
+
+import axios from 'axios';
+
+import db from '../helpers/postgres';
+
+type Subscribers = {
+  url: string;
+  spaces?: string[];
+}[];
+
+type EventsDB = {
+  event: string;
+  expire: number;
+  id: string;
+  space: string;
+};
+
+const DELAY = 5;
+const INTERVAL = 30;
+const SERVICE_EVENTS_ACTIVE = parseInt(process.env.SERVICE_EVENTS || '0');
+
+/**
+ * Gets subscribers JSON by environment (e.g. `local`, `dev`, `prod`)
+ * from `./subscribers/`.
+ *
+ * @returns `Subscribers`
+ */
+const getSubscribersFromFile = async (): Promise<Subscribers> => {
+  const { default: subscribers } = await import(
+    `./subscribers/${process.env.ENV}.json`
+  );
+
+  return subscribers;
+};
+
+async function sendEvent(event, to) {
+  const res = await axios.post(to, event);
+
+  // Attempt to get any JSON response
+  try {
+    return await res;
+  } catch (error) {
+    return undefined;
+  }
+}
+
+async function processEvents() {
+  const ts = parseInt((Date.now() / 1e3).toFixed()) - DELAY;
+
+  const events = await db.query<EventsDB, [number]>(
+    'SELECT * FROM events WHERE expire <= $1',
+    [ts]
+  );
+
+  const subscribers = await getSubscribersFromFile();
+
+  console.log('Process event start', ts, events.rows.length);
+
+  for (const event of events.rows) {
+    Promise.all(
+      subscribers
+        .filter(
+          subscriber =>
+            !subscriber.spaces || subscriber.spaces.includes(event.space)
+        )
+        .map(subscriber => sendEvent(event, subscriber.url))
+    )
+      .then(() => console.log('Process event done'))
+      .catch(e => console.log('Process event failed', e));
+
+    try {
+      await db.query<any, [string, string]>(
+        'DELETE FROM events WHERE id = $1 AND event = $2',
+        [event.id, event.event]
+      );
+
+      console.log(`Event sent ${event.id} ${event.event}`);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+}
+
+if (SERVICE_EVENTS_ACTIVE) {
+  setInterval(async () => await processEvents(), INTERVAL * 1e3);
+}
