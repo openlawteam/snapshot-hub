ccca3bbb21461f8a990756569dc75b80cfda56b9
Yousuf Haque <yousuf.s.haque@gmail.com>
2023-10-11 22:15:36 -0400

    feat(persistence-interface): Introduce toggle for using mongo persistence

--- server/repositories/events-repository.ts
+++ server/repositories/events-repository.ts
@@ -1,5 +1,7 @@
 import { postgresEventsRepository } from '../helpers/adapters/postgres';
 import { Event } from '../models/event.js';
+import { useMongoPersistence } from '../index.js';
+import { mongoEventsRepository } from './mongo/mongo-events-repository.js';
 
 export type EventsRepository = {
   getExpiredEvents: (timestamp: number) => Promise<Event[]>;
@@ -8,17 +10,19 @@ export type EventsRepository = {
     EVENT_ID: string,
     space: string,
     timestamp: number
-  ) => Promise<void>,
+  ) => Promise<void>;
   insertStartedProposal: (
     EVENT_ID: string,
     space: string,
     timestamp: number
-  ) => Promise<void>
+  ) => Promise<void>;
   insertProposalEnd: (
     EVENT_ID: string,
     space: string,
     timestamp: number
-  ) => Promise<void>
+  ) => Promise<void>;
 };
 
-export const eventsRepository: EventsRepository = postgresEventsRepository;
+export const eventsRepository: EventsRepository = useMongoPersistence
+  ? mongoEventsRepository
+  : postgresEventsRepository;
--- server/repositories/offchain-proofs-repository.ts
+++ server/repositories/offchain-proofs-repository.ts
@@ -2,14 +2,16 @@ import {
   OffchainProof,
   postgresOffchainProofsRepository
 } from '../helpers/adapters/postgres';
+import { useMongoPersistence } from '../index.js';
+import { mongoOffchainProofsRepository } from './mongo/mongo-offchain-proofs-repository.js';
 
 export type OffchainProofsRepository = {
   getOffchainProof: (
     space: string,
     merkleRoot: string
   ) => Promise<OffchainProof[]>;
-  saveOffchainProof: (
-    offchainProof: OffchainProof
-  ) => Promise<void>;
+  saveOffchainProof: (offchainProof: OffchainProof) => Promise<void>;
 };
-export const offchainProofsRepository: OffchainProofsRepository = postgresOffchainProofsRepository;
+export const offchainProofsRepository: OffchainProofsRepository = useMongoPersistence
+  ? mongoOffchainProofsRepository
+  : postgresOffchainProofsRepository;
